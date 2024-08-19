49e4b7bfa38bdcd42989954e4bb99173e55141f5
phu.nguyen <phu.nguyen@localhost>
2024-08-19 12:21:49 +0700

    new tsc

---
+++ Test Suites/New Test Suite Collection.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>New Test Suite Collection</name>
+   <tag></tag>
+   <delayBetweenInstances>0</delayBetweenInstances>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Web Desktop</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>true</requireConfigurationData>
+            <runConfigurationData>
+               <entry>
+                  <key>testCloudMobileDevice</key>
+                  <value>{
+  &quot;id&quot;: &quot;google_pixel_7_pro&quot;,
+  &quot;isTablet&quot;: false,
+  &quot;manufacturer&quot;: [
+    &quot;Google&quot;
+  ],
+  &quot;name&quot;: &quot;Google Pixel 7 Pro&quot;,
+  &quot;osVersion&quot;: &quot;15&quot;,
+  &quot;mobileDeviceId&quot;: &quot;Google_Pixel_7_Pro_15_beta_real_us&quot;,
+  &quot;capability&quot;: {},
+  &quot;os&quot;: &quot;Android&quot;,
+  &quot;platformName&quot;: &quot;ANDROID&quot;,
+  &quot;timeout&quot;: 15,
+  &quot;usingTunnel&quot;: false
+}</value>
+               </entry>
+               <entry>
+                  <key>testCloudSessionType</key>
+                  <value>MOBILE_NATIVE</value>
+               </entry>
+               <entry>
+                  <key>testCloudOverrideMobileApplication</key>
+                  <value>{&quot;id&quot;:&quot;730bc8ef-2c04-4478-81b1-5c95561035e6&quot;,&quot;organizationId&quot;:&quot;1530506&quot;,&quot;name&quot;:&quot;Smart Delta&quot;,&quot;platform&quot;:&quot;ANDROID&quot;,&quot;status&quot;:&quot;READY&quot;,&quot;size&quot;:64174029,&quot;storagePath&quot;:&quot;ebeab4e4-4b38-4488-82e4-3424f78b8969&quot;,&quot;uploadedDate&quot;:&quot;2024-08-08T06:27:33.639377&quot;,&quot;createdDate&quot;:&quot;2024-08-08T06:27:33.639377&quot;,&quot;updatedDate&quot;:&quot;2024-08-08T07:46:06.809427&quot;,&quot;fileName&quot;:&quot;SmartDelta_v_1.0.30.63_LIVE.apk&quot;,&quot;metadata&quot;:{&quot;identifier&quot;:&quot;com.jainam.smartdelta&quot;,&quot;name&quot;:&quot;Smart Delta&quot;,&quot;version&quot;:&quot;1.0.30&quot;}}</value>
+               </entry>
+            </runConfigurationData>
+            <runConfigurationId>TestCloud</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/15sec</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
